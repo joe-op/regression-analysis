7d09d3a4687b507cb7f0e94d17f37fe72337beb2
joe-op <josef@joeopseth.com>
2016-10-07 05:17:01 -0500

    Add Function section at end; do problem 1-1

--- assignment-4/transform.R
+++ assignment-4/transform.R
@@ -1,5 +1,8 @@
 anvp <- function(y,x) {
-    return(anova(lm(y~x))$Pr[1])
+    return(round(anova(lm(y~x))$Pr[1], 4))
+}
+sr <- function(y,x) {
+    return(round(summary(lm(y~x))$r.squared, 4))
 }
 
 transform <- function(y,x) {
@@ -7,6 +10,7 @@ transform <- function(y,x) {
 
     name <- c("x^2","sqrt(x)","1/x","log(x,10)","y^2","sqrt(y)","1/y","log(y,10)")
     p <- vector(mode="numeric", length=8)
+    r2 <- vector(mode="numeric", length=8)
 
     p[1] <- anvp(y,x^2)
     p[2] <- anvp(y,sqrt(x))
@@ -16,7 +20,16 @@ transform <- function(y,x) {
     p[6] <- anvp(sqrt(y),x)
     p[7] <- anvp(1/y,x)
     p[8] <- anvp(log(y,10),x)
+
+    r2[1] <- sr(y,x^2)
+    r2[2] <- sr(y,sqrt(x))
+    r2[3] <- sr(y,1/x)
+    r2[4] <- sr(y,log(x,10))
+    r2[5] <- sr(y^2,x)
+    r2[6] <- sr(sqrt(y),x)
+    r2[7] <- sr(1/y,x)
+    r2[8] <- sr(log(y,10),x)
      
-    result <- data.frame(name, p, stringsAsFactors=F)
+    result <- data.frame(name, p, r2, stringsAsFactors=F)
     return(result)
 }
